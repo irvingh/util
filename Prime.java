d79f9581b359ff268d48d8f38654142a4aa6d064
irving <irving.hornic@gmail.com>
2018-08-29 20:51:32 +0300

    CodeKnight 1.0

--- Prime.java
+++ Prime.java
@@ -1,20 +1,31 @@
 public class Prime {
-    private int n;
+    private int max;
     private int[] p;
-    public  Prime (int n) {
-        this.n = n;
-        this.p = new int[n+1];
-        for (int factor=2; factor*factor<=n; factor++) {
-            if (p[factor]==0) {
+
+    Prime(int n) {
+        this.max = n;
+        this.p = new int[n + 1];
+        for (int factor = 2; factor*factor <= n; factor++) {
+            if (p[factor] == 0) {
                 p[factor] = 1;
-                for (int j=factor; factor*j <= n; j++) {
+                for (int j = factor; factor*j <= n; j++) {
                     p[factor*j] = -1;
                 }
             }
         }
     }
+
     public boolean isPrime(int a) {
-        if (a<2 || a>=n) return false;
-        return (this.p[a]==1);
+        if ( a < 2 || a >= max) return false;
+        return (this.p[a] == 1);
+    }
+
+    public int next(int n) {
+        while (n <= this.max) {
+            if (p[n] == 1)
+                return n;
+            n++;
+        }
+        return -1;
     }
 }
